4ecfa957ad194c9612186fa0ed3f0508376eed07
alshohatee <alshohateeaseel@gmail.com>
2022-01-11 23:19:14 -0500

    create a function that unfill the colors of the elements fixed #8

--- index.js
+++ index.js
@@ -122,6 +122,14 @@ function setId() {
 }
 
 // fill all uncolored cells with the currently selected color
+function UnfillcoloredCells() {
+    let lengthRowInFirstCol = gridContainer[0].getElementsByClassName("col")[0].childElementCount
+    for (let j = 0; j < lengthRowInFirstCol; j++) {
+        document.getElementsByClassName("row")[j].setAttribute("style", "background-color: white;")
+
+    }
+
+}
 // fill all cells with the currently selected color
 // clear all cells / restore all cells to their original / initial color
 // click and hold(mouseover) from a single cell(start) to a different cell(end) such that all affected / hovered - over cells from start to end change to the currently selected color
